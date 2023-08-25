49a9039b55ab3b3fd31a14cbeaf2ff3d5c4591e4
Conor H <conorhartigan13@gmail.com>
2023-08-25 23:41:14 +0100

    Add files via upload

---
+++ secret_santa.py
@@ -0,0 +1,43 @@
+import random
+import smtplib
+from email.mime.text import MIMEText
+
+EMAIL_HOST = 'host'
+EMAIL_PORT = 587
+EMAIL_USER = 'email@email.com'
+EMAIL_PASSWORD = 'password'
+
+participants = [
+    {'name': 'Jason', 'email': 'jason.p.p.hanafin@gmail.com'},
+    {'name': 'Paddy', 'email': 'paddypizza@gmail.com'},
+    {'name': 'Conor', 'email': 'conorhartigan13@gmail.com'},
+    {'name': 'Ryan', 'email': 'ryan@email.com'},
+    {'name': 'Pepi', 'email': 'elenahorgan2@gmail.com'},
+    {'name': 'Luna', 'email': 'unlesslana@gmail.com'},
+]
+
+random.shuffle(participants)
+
+assignments = {}
+for i in range(len(participants)):
+    giver = participants[i]
+    receiver = participants[(i + 1) % len(participants)]
+    assignments[giver['name']] = receiver['name']
+
+for giver, receiver in assignments.items():
+    message = f"Hello {giver}!\n\nYou are the Secret Santa for {receiver}.\nPlease buy a thoughtful gift for them!\n\nBest regards,\nSecret Santa Organizer"
+
+    msg = MIMEText(message)
+    msg['Subject'] = 'Secret Santa Assignment'
+    msg['From'] = EMAIL_USER
+    msg['To'] = giver
+
+    try:
+        server = smtplib.SMTP(EMAIL_HOST, EMAIL_PORT)
+        server.starttls()
+        server.login(EMAIL_USER, EMAIL_PASSWORD)
+        server.sendmail(EMAIL_USER, [giver], msg.as_string())
+        server.quit()
+        print(f"Email sent to {giver}")
+    except Exception as e:
+        print(f"Error sending email to {giver}: {e}")
